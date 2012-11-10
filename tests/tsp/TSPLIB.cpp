c6f6760a64cf5dbcd644ae5d533513d2e74de7e3
Grzegorz Prusak <pompon.pompon@gmail.com>
2012-11-10 11:54:06 +0100

    added to tests triange inequality checking for TSPLIB explicit matrices

--- tests/tsp/TSPLIB.cpp
+++ tests/tsp/TSPLIB.cpp
@@ -46,3 +46,23 @@ TEST(TSPLIB_Directory,dist)
 		EXPECT_EQ(fitness,fitnesses[i]);
 	}
 }
+
+TEST(TSPLIB,triange_inequality)
+// check triangle inequality in TSPLIB explicit matrices
+{
+	std::string path = "./TSPLIB/symmetrical";
+	tsp::TSPLIB_Directory dir(path);
+	tsp::TSPLIB_Matrix mtx;
+	for(auto &g : dir.graphs)
+	{
+		g.load(mtx);
+		if(mtx.dist_) continue;
+		bool ok = 1;
+		for(size_t i=0; i<mtx.size1(); ++i)
+			for(size_t j=0; j<mtx.size1(); ++j)
+				for(size_t k=0; k<mtx.size1(); ++k)
+					if(mtx(i,j)>mtx(i,k)+mtx(k,j))
+					{ i = j = k = mtx.size1(); ok = 0; }
+		std::cout << format("% %",g.filename,ok?"OK":"INVALID") << std::endl;
+	}
+}
