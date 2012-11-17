2042fb7a8b1d94a42a1faedb5859a38d72720b7a
Łukasz Solak <solak.lukas@gmail.com>
2012-11-18 00:53:26 +0100

    function invoked on example graph

---
+++ steiner/SteinerForest.h
@@ -0,0 +1,12 @@
+#ifndef _STEINER_FOREST_H
+#define _STEINER_FOREST_H
+
+#include <boost/graph/adjacency_matrix.hpp>
+
+template <typename G, typename S>
+void SteinerForest(const G& graph, const S& sets)
+{
+
+}
+
+#endif /* _STEINER_FOREST_H */
---
+++ steiner/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "SteinerForest.h"
+#include <boost/graph/adjacency_matrix.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/graph/graph_utility.hpp>
+
+struct vertex_set_t {
+    typedef boost::vertex_property_tag type;
+};
+
+struct Edge
+{
+    int u;
+    int v;
+    int weight;
+};
+
+int main()
+{
+    //example graph - Vazirani, page 202
+    const int vertexCount = 6;
+    typedef boost::adjacency_matrix<boost::undirectedS,
+                                    boost::no_property,
+                                    boost::property<boost::edge_weight_t, int> > graph_t;
+    graph_t graph(vertexCount);
+
+    const char * names = "usabvt";
+
+    const int edgeCount = 9;
+    Edge edges[edgeCount] = {{0, 1, 16},
+                             {0, 2, 6},
+                             {1, 2, 12},
+                             {1, 2, 6},
+                             {2, 3, 9},
+                             {3, 4, 6},
+                             {0, 4, 20},
+                             {4, 5, 19},
+                             {3, 5, 12}};
+
+    for(int i = 0; i < edgeCount; ++i)
+    {
+        boost::add_edge(edges[i].u, edges[i].v, edges[i].weight, graph);
+    }
+
+    //vertices sets, 0 - none
+    typedef boost::property_map<graph_t, boost::vertex_index_t>::type vertex_set_map_t;
+    vertex_set_map_t v = get(boost::vertex_index, graph);
+    int sets[vertexCount] = {1, 2, 0, 0, 1, 2}; //0 - none
+    typedef boost::iterator_property_map<int *, vertex_set_map_t, int, int&> sets_t;
+    sets_t sets_map(sets, v);
+//    std::cout << get(boost::edge_weight, graph, edge(0, 1, graph).first) << std::endl;
+//    std::cout << get(sets_map, 1) << std::endl;
+//    boost::print_graph(graph, names);
+
+    SteinerForest<graph_t, sets_t>(graph, sets_map);
+
+    return 0;
+}
+
