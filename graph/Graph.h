0fb762de5ea7b4e4d1bad9910a617f408d88cb28
Łukasz Solak <solak.lukas@gmail.com>
2013-03-07 03:24:27 +0100

    Graph.h astyled and cpplinted.

--- graph/Graph.h
+++ graph/Graph.h
@@ -1,11 +1,17 @@
-#ifndef _GRAPH_H
-#define _GRAPH_H
+#ifndef GRAPH_GRAPH_H_
+#define GRAPH_GRAPH_H_
 
 namespace Graph
 {
-    struct directed { enum { is_directed = true }; };
-    struct undirected { enum { is_directed = false }; };
-    struct unweighted {};
-}
+  struct directed
+  {
+    enum { is_directed = true };
+  };
+  struct undirected
+  {
+    enum { is_directed = false };
+  };
+  struct unweighted {};
+}  // namespace Graph
 
-#endif /* _GRAPH_H */
+#endif  // GRAPH_GRAPH_H_
