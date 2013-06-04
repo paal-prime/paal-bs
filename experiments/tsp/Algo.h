faff9b8efc305e82b6a1de35ff6dd8e0c2098727
Mateusz Machalica <mateuszmachalica@gmail.com>
2013-06-04 03:32:30 +0200

    [WIP] TSP algorithms comparison stub.

---
+++ experiments/tsp/Algo.h
@@ -0,0 +1,111 @@
+#ifndef EXPERIMENTS_TSP_ALGO_H_
+#define EXPERIMENTS_TSP_ALGO_H_
+
+#include <random>
+
+#include <paal/ProgressCtrl.h>
+#include <mcts/MonteCarloTree.h>
+#include <mcts/Policy.h>
+#include <tsp/MCTS_tsp.h>
+#include <tsp/TSPLIB.h>
+#include "paal/search.h"
+#include "paal/StepCtrl.h"
+#include "tsp/TwoOptWalker.h"
+#include "tsp/util.h"
+
+std::mt19937 random_(9823429);
+
+typedef tsp::TSPLIB_Matrix Matrix;
+typedef tsp::TSPState<Matrix> State;
+
+/** @brief [implements Algo] */
+struct LocalSearchAlgo
+{
+  LocalSearchAlgo(Matrix &_matrix, size_t _it) : matrix(_matrix), it(_it) {}
+  Matrix &matrix;
+  size_t it; //iterations
+
+  template<typename Logger, typename StepCtrl, typename Cycle>
+  double run_(Logger &logger, StepCtrl &step_ctrl, const Cycle &cycle) const
+  {
+    tsp::TwoOptWalker<Matrix> walker(matrix, cycle);
+    paal::IterationCtrl progress_ctrl(it);
+    //paal::TimeAutoCtrl progress_ctrl(15);
+    //double begin = paal::realtime_sec();
+    paal::search(walker, random_, progress_ctrl, step_ctrl, logger);
+    //std::cout << "final cost: " << tsp::fitness(matrix,walker.cycle) << " " << walker.current_fitness() << std::endl;
+    //double end = paal::realtime_sec();
+    //std::cout << format("time=%\n",end-begin);
+    return walker.current_fitness();
+  }
+};
+
+struct HillAlgo : LocalSearchAlgo
+{
+  HillAlgo(Matrix &_matrix, size_t _it) : LocalSearchAlgo(_matrix, _it) {}
+
+  template<typename Logger> double run(Logger &logger) const
+  {
+    std::vector<size_t> cycle;
+    tsp::cycle_shuffle(cycle, matrix.size1(), random_);
+    //std::cout << "initial cost: " << tsp::fitness(matrix,cycle) << std::endl;
+    paal::HillClimb step_ctrl;
+    return run_(logger, step_ctrl, cycle);
+  }
+};
+
+struct AnneAlgo : LocalSearchAlgo
+{
+  AnneAlgo(Matrix &_matrix, size_t _it, double _t1) :
+    LocalSearchAlgo(_matrix, _it), t1(_t1) {}
+  double t1;
+
+  template<typename Logger> double run(Logger &logger) const
+  {
+    std::vector<size_t> cycle;
+    tsp::cycle_shuffle(cycle, matrix.size1(), random_);
+    paal::Annealing step_ctrl(
+      tsp::fitness(matrix, cycle) / matrix.size1(), t1);
+    return run_(logger, step_ctrl, cycle);
+  }
+};
+
+/** @brief [implements Algo] */
+template<typename Policy> struct MCTSAlgo
+{
+  State *state_;
+  Policy policy_;
+  size_t full_search_;
+  double samples_ratio_;
+
+  MCTSAlgo(Policy policy, size_t samples_ratio = 500, size_t full_search = 9) :
+    state_(NULL), policy_(policy), full_search_(full_search),
+    samples_ratio_(samples_ratio) {}
+
+  template<typename Logger> double run(Logger &logger) const
+  {
+    mcts::MonteCarloTree<State, Policy> mct(*state_, policy_);
+    while (!mct.root_state().is_terminal())
+    {
+      if (mct.root_state().left_decisions() > full_search_)
+      {
+        size_t samples = mct.root_state().left_decisions() * samples_ratio_;
+        paal::IterationCtrl ctrl(samples);
+        auto move = mct.search(ctrl);
+        mct.apply(move);
+      }
+      else
+      {
+        mct.root_state().exhaustive_search_min();
+      }
+    }
+    return mct.root_state().cost_;
+  }
+};
+
+template<typename Policy> MCTSAlgo<Policy> make_algo(Policy policy)
+{
+  return MCTSAlgo<Policy>(policy);
+}
+
+#endif  // EXPERIMENTS_TSP_ALGO_H_
---
+++ experiments/tsp/tsp_comparison.cpp
@@ -0,0 +1,64 @@
+#include <random>
+#include <algorithm>
+#include <vector>
+#include <iostream>  // NOLINT(readability/streams)
+
+#include <paal/GridTable.h>
+
+#include <result_dir.h>
+#include <format.h>
+
+#include "experiments/tsp/Algo.h"
+
+int main(int argc, char **argv)
+{
+  using namespace mcts;
+  Dir resdir(argc, argv);
+
+  paal::GridTable table;
+  table.push_algo("Optimal");
+  table.push_algo("HillClimb");
+  table.push_algo("MonteCarloSearch");
+  table.push_algo("Christofides");
+
+  tsp::TSPLIB_Directory dir("./TSPLIB/symmetrical/");
+  size_t it = 0;
+  std::vector<int> iterlimit = {1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 10000000, };
+  std::vector<int> samplelimit = {1000, 400, 200, 70, 32, 11, 0};
+  for (auto gid : {"eil51", "eil76", "eil101", "pr152", "d198", "a280", "brd14051", })
+  {
+    Matrix matrix;
+    dir.graphs[gid].load(matrix);
+
+    auto hillclimb = HillAlgo(matrix, iterlimit[it]);
+
+    MCTSAlgo<PolicyRandMean<std::mt19937> > mctsalgo(
+      PolicyRandMean<std::mt19937>(random_), samplelimit[it]);
+    State state(matrix, matrix.size1() / 4);
+    mctsalgo.state_ = &state;
+
+    table.columns.push_back(format("%", gid));
+    table.records[0].results.push_back(dir.graphs[gid].optimal_fitness);
+    double start;
+    start = paal::realtime_sec();
+    table.records[1].test(hillclimb);
+    std::cerr << "runtime " << paal::realtime_sec() - start << std::endl;
+
+    start = paal::realtime_sec();
+    if (samplelimit[it])
+      table.records[2].test(mctsalgo);
+    else
+      table.records[2].results.push_back(1. / 0.);
+    std::cerr << "runtime " << paal::realtime_sec() - start << std::endl;
+
+    start = paal::realtime_sec();
+    table.records[3].results.push_back(dir.graphs[gid].optimal_fitness);
+    std::cerr << "runtime " << paal::realtime_sec() - start << std::endl;
+
+    std::cerr << "done " << gid << std::endl;
+    it++;
+  }
+  std::ofstream tex(resdir("eil_graphs.tex"));
+  table.dump_tex(tex);
+  return 0;
+}
